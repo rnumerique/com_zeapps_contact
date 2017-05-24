9eb90ace8d92bc2552ddbb9923ede8b6f6d1aae5
ThomasSauques <developpeur@Nicolass-Mac-mini.local>
2017-05-24 17:36:08 +0200

    model update for marketing module

--- models/Zeapps_country_lang.php
+++ models/Zeapps_country_lang.php
@@ -1,3 +1,9 @@
 <?php
 class Zeapps_country_lang extends ZeModel {
+    public function get_select(){
+        return $this->database()->select('zeapps_country_lang.name as label,
+                                        zeapps_country_lang.id_country as value')
+            ->table('zeapps_country_lang')
+            ->result();
+    }
 }
